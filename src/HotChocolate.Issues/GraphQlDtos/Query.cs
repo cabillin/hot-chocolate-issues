c7b343a7f527c79f23db58ec58b64c14d7411072
Catherine Billings <cabillin@microsoft.com>
2024-11-06 12:06:29 -0800

    make it better

--- src/HotChocolate.Issues/GraphQlDtos/Query.cs
+++ src/HotChocolate.Issues/GraphQlDtos/Query.cs
@@ -6,6 +6,11 @@ namespace HotChocolate.Issues.GraphQlDtos
 {
     public class Query
     {
+        public string Ping()
+        {
+            return "Hello World";
+        }
+
         public async Task<List<ParentObject>> GetParents(IFakeDataService dataService)
         {
             return await dataService.GetAllParents();
